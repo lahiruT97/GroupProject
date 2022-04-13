0a06bc4bbd2cb3ac836ef172d964e63a14288491
lahiruT97 <lahiruthraka97@gmail.com>
2022-04-13 12:42:03 +0530

    test3

---
+++ future.php
@@ -0,0 +1,144 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/mediPlusDesign.css">
+        <script>
+            function validationMail(){
+                var email = document.forms["myform"]["mail"].value;
+                var type = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                var lblError = document.getElementById("lblError");
+                lblError.innerHTML = "";
+                if(!type.test(email))
+                {
+                    lblError.innerHTML = "Invalid email address";
+                    return false;
+                }
+                return true;
+            }
+            function validation(){
+                var cmd = document.forms["myform"]["cmd"].value;
+                var len = cmd.length;
+                if(len>100){
+                    alert("Message must be less than 100 letters");
+                    return false;
+                }
+                else if(cmd="Message*"){
+                    alert("Please add your message");
+                    return false;
+                }
+		        return true;
+            }
+        </script>
+    </head>
+    <body>
+        <div class="main" style="background-image: url(future.jpg);">
+            <div class="text">
+                Future Saver
+            </div>
+        </div>
+    <div class="row">
+        <div class="topic"><br><br>
+            <h2>What is Future Saver?</h2>
+        </div>
+        <div class="slogon">
+            <p class="sloganText">Insurance for all from the largest and strongest 
+            National Insurer</p>
+        </div>
+    </div>
+    <div class="det">
+        <p class="details">
+        Whatever happens next,&nbsp; you can be future-ready with Illuminating Life Future Saver. This exclusive plan is your 
+        smartest path to a life of prosperity and peace of mind.<br>
+        Future Saver helps you build an ample wealth fund, continuously boosted with generous returns,&nbsp; to achieve your future 
+        goals. Further,&nbsp; it provides strong life protection to secure your loved ones against uncertainties.<br>
+        Most uniquely,&nbsp; Future Saver delivers these invaluable long-term benefits without long-term costs. 
+        It requires you to save diligently,&nbsp; but only for a short period when your financial stability is at its peak.
+        </p><br>
+    </div>
+    <h2>Benefits and Eligibility</h2><br>
+    <div class="benefits">
+        <div class="column1">
+            <img src="image/benefit5_5.jpg" width="90px" height="90px"><br>
+            <h3>Growing Wealth Fund ensuring an Attractive Lump Sum at the Maturity</h3>
+               <p class="text2">Future Saver ensures a growing Wealth Accumulation Fund,&nbsp; which is built on monthly dividend rates declared 
+                   by the company for the Basic Premium paid by you. </p>
+        
+        </div>
+        <div class="column2">
+            <img src="image/benefit4_4.jpg" width="90px" height="90px"><br>
+            <h3>Long Term Protection for a Short Term Commitment</h3>
+            <p class="text2">
+                You only need to pay premium for either 5 years or 10 years to remain protected for 10,&nbsp; 15,&nbsp; 20 or 25 years,&nbsp; 
+                based on your personal choice.
+            </p>
+        </div>
+        <div class="column3">
+            <img src="image/benefit1.jpg" width="90px" height="90px">
+            <h3>Life Protection Benefit</h3>
+            <p class="text2">
+                You will receive a life insurance cover with an affordable premium that ensures a promising tomorrow for your loved ones in 
+                the case of your unfortunate demise. In the event of your death during the term of the Policy,&nbsp; your beneficiaries 
+                will receive: Life Cover (Basic Sum Assured + Family Protection Benefit)
+            </p>
+        </div>
+        <div class="column4">
+            <img src="image/benefit8_8.jpg" width="90px" height="90px">
+            <h3>Premium Assurance Benefit</h3>
+            <p class="text2">
+                In the unfortunate event of death,&nbsp; Illuminating Life will continue the payment of Basic Premium for your Wealth 
+                Fund until the end of the premium paying term and will build the fund during your absence for your loved ones. 
+            </p>
+        </div>
+        <div class="column5">
+            <img src="image/benefit9_9.jpg" width="90px" height="90px">
+            <h3>No Claim Bonus</h3>
+            <p class="text2">
+                A No Claim Bonus that increases the basic sum assured by 5% up to a maximum of 50% for every claim-free year.
+            </p>
+        </div>
+        <div class="column6">
+            <img src="image/benefit6_6.jpg" width="90px" height="90px">
+            <h3>Loyalty Rewards</h3>
+            <p class="text2">
+                Paying premiums on time demonstrates your continuous financial commitment. This is recognized through loyalty rewards,&nbsp; 
+                which are added to your wealth fund at specified intervals during the policy term. It's another way you'll obtain an 
+                attractive wealth fund at maturity.
+            </p>
+        </div>
+        <div class="column7">
+            <img src="image/benefit7_7.jpg" width="90px" height="90px">
+            <h3>Eligibility</h3>
+            <p class="text2">
+                The policy can be obtained by adults above the age of 55 years with the cover ceasing at the age of 79.<br>
+            </p>
+        </div>
+    </div>
+    <div class="row2">
+        <div class="col1" style="background-color: #000080;">
+            <div class="row2Topic">
+                <h2 style="color: white;">Why should I select Illuminating Life Insurance as My Insurance Partner?</h2>
+            </div>
+            <div class="row2Details">
+                <p class="details" style="color: white;">
+                    Illuminating Life Insurance is the pioneer insurance company in the island with a history spanning 57 years. 
+                    Being backed by the state has added to its strength and credibility while it has the most experienced technical knowledge 
+                    base. Its Life Fund in excess of LKR 134 billion and asset base of over LKR 235 billion are unmatched in the industry.
+                </p>
+            </div>
+        </div>
+        <div class="col2" style="margin-left:40px;">
+            <h2 style="margin-top: -35px; margin-left: 30px;">Quick Inquiry</h2>
+            <form name="myform" action="insert.php" method="post" onsubmit="validation()"><br>
+                <input type="text" name="cat_name" value="Future Saver" readonly><br><br>
+                <input type="mail" name="mail" value="E-mail*" onkeypress="validationMail()"><br><br>
+                <span id="lblError" style="color: red"></span>
+                <textarea name="cmd" rows="6" cols="59" >Message*</textarea><br><br>
+                <input type="submit" name="submit">
+            </form>
+            <div class="apply">
+                <a href=""><h2 style="color: white;">Apply now...</h2></a>
+            </div>
+        </div>
+    </div>
+    <script src="script/mediPlusScript.js"></script>
+    </body>
+</html>
---
+++ table.php
@@ -0,0 +1,27 @@
+<?php
+    $servername="localhost";
+    $username="root";
+    $password="madusank17112";
+    $dbname= "group12";
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+
+    if($conn->connect_error){
+        die("connectin failed :" .$conn->connect_error);
+    }
+
+    $sql2 = "CREATE TABLE inquiry
+            (id INT AUTO_INCREMENT PRIMARY KEY,
+            cat_name VARCHAR(20),
+            email VARCHAR(20),
+            cmd VARCHAR(100))";
+        
+    if($conn->query($sql2)===true){
+        echo "Table crated successfully";
+    }
+    else{
+        echo "failed creating table :".$conn->error;
+    }
+
+    $conn->close();
+?>
