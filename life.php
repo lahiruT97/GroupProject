5275d011aa3df513d906b0878606390abbf96201
lahiruT97 <101711044+lahiruT97@users.noreply.github.com>
2022-05-06 23:35:02 +0530

    last update for life insurance page

--- life.php
+++ life.php
@@ -3,9 +3,43 @@
         <meta charset="UTF-8">
 	    <meta name="viewport" content="width-device-width, initial-scale=1.0">
 	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css\lifeDesign.css">
+        <link rel="stylesheet" href="css/lifeDesign.css">
+
+        <meta charset="utf-8" />
+    
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta content="Illuminationg - Insurance Company Website" name="description" />
+        <meta content="" name="keywords" />
+        <meta content="" name="author" />
+
+ 
+
+   
+    <link href="css/jpreloader.css" rel="stylesheet" type="text/css">
+    <link id="bootstrap" href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-grid" href="css/bootstrap-grid.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-reboot" href="css/bootstrap-reboot.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.theme.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.transitions.css" rel="stylesheet" type="text/css" />
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery.countdown.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+    <link id="colors" href="css/colors/scheme-01.css" rel="stylesheet" type="text/css" />
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" />
+
+  
+    <link rel="stylesheet" href="revolution/css/settings.css" type="text/css">
+    <link rel="stylesheet" href="revolution/css/layers.css" type="text/css">
+    <link rel="stylesheet" href="revolution/css/navigation.css" type="text/css">
     </head>
     <body>
+
+    <?php include 'includes/header.php' ?>
+
         <div class="main" style="background-image: url(life1.jpg)";>
             <div class="text">
                 Life insurence plan
@@ -13,13 +47,13 @@
         </div>
         <div class="row0">
             <div class="column0">
-                <h2>Life Insurence</h2>
+                <h2 class="lahiru_h2">Life Insurence</h2>
             </div>
             <div class="column01">
-                <h2>Understanding Life Insurence</h2>
+                <h2 class="lahiru_h2">Understanding Life Insurence</h2>
             </div>
         </div>
-            <div class="row">
+            <div class="row1">
                 <div class="column1">
                     <p class="details1">
                         Life Insurance is not just an intelligent decision you make to help you during times of uncertainty. At Illuminating Life insurance, &nbsp;
@@ -27,84 +61,84 @@
                         goals and dreams with total peace of mind.
                     </p>
                     <div class="col1">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Medi Plus</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 Medi Plus Health Insurance plan covers a range of health insurance needs at an affordable price and 
                                 comes with a special no claim bonus, which increases the sum insured for every claim-free...
                             </div>
                             <div class="button">
-                                <a href="mediPlus.php">Read More</a>
+                                <a class="lahiru_link" href="mediPlus.php">Read More</a>
                             </div>
                           </div>
                     </div>
                     <div class="col2">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Surgical Plan</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 For financial coverage in the event of an unexpected surgery, the Illuminating insurence Surgery Plan 
                                 provides you with a comprehensive policy that covers surgery costs up to a pre-specified 
                                 maximum limit. The...
                             </div>
                             <div class="button">
-                                <a href="surgical.php">Read More</a>
+                                <a class="lahiru_link" href="surgical.php">Read More</a>
                             </div>
                           </div>
                     </div>
                     <div class="col3">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Hospital cash Plan</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 A unique and innovative health plan that provides a fixed amount as cover for each 
                                 day you are hospitalized, irrespective of the actual bills or expenses. This affords...
                             </div>
                             <div class="button">
-                                <a href="hospital.php">Read More</a>
+                                <a class="lahiru_link" href="hospital.php">Read More</a>
                             </div>
                           </div>
                     </div>
                     <div class="col4">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Future Saver</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 Whatever happens next,&nbsp; you can be future-ready with Iluuminating Life insurence Future Saver. 
                                 This exclusive plan is your smartest path to a life of prosperity and peace of mind.
                                 Future Saver helps you build an...
                             </div>
                             <div class="button">
-                                <a href="future.php">Read More</a>
+                                <a class="lahiru_link" href="future.php">Read More</a>
                             </div>
                           </div>
                     </div>
                     <div class="col5">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Saubhagya</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 In today's world,&nbsp; women play a crucial role as mothers,&nbsp; breadwinners,&nbsp;
                                  daughters,&nbsp; leaders and strong independent personalities who strive to win the world.
                                   The sacrifices they make daily are many...
                             </div>
                             <div class="button">
-                                <a href="Saubhagya.php">Read More</a>
+                                <a class="lahiru_link" href="Saubhagya.php">Read More</a>
                             </div>
                           </div>
                     </div>
                     <div class="col6">
-                        <img src="image\logo1.png" class="logo">
+                        <img src="image\logo1.png" class="logo1">
                         <p class="cgrName">Medi 60</p>
-                        <div class="overlay">
+                        <div class="overlay1">
                             <div class="text1">
                                 Illuminating insurance Medi 60 is a health insurance product which 
                                 Covering a range of health insurance needs of senior citizens (Age 60) 
                                 in the country at a highly affordable price. You can...
                             </div>
                             <div class="button">
-                                <a href="medi60.php">Read More</a>
+                                <a class="lahiru_link" href="medi60.php">Read More</a>
                             </div>
                           </div>
                     </div>
@@ -125,44 +159,44 @@
                         So let us assist you to select the right plan for you as mention earlier.
                     </p><br>
 
-                    <h2>Why Should You Get A Life Insurence Cover?</h2>
+                    <h2 class="lahiru_h2">Why Should You Get A Life Insurence Cover?</h2>
                     <div class="whySelect">
                         <ul>
-                            <li>In the unfortunate event of your sudden demise,&nbsp; the future of your 
+                            <li class="lahiru_li">In the unfortunate event of your sudden demise,&nbsp; the future of your 
                                 loved ones will be free of worry.</li>
-                            <li>Coverage of critical illnesses,&nbsp; should you or a family member 
+                            <li class="lahiru_li">Coverage of critical illnesses,&nbsp; should you or a family member 
                                 contract a serious illness.</li>
-                            <li>A monthly income in the event you or a dependant were to fall victim 
+                            <li class="lahiru_li">A monthly income in the event you or a dependant were to fall victim 
                                 to a total or permanent disability.</li>
-                            <li>To release you from your loan debts.</li>
-                            <li>To enjoy your retirement without being a burden on anyone.</li>
+                            <li class="lahiru_li">To release you from your loan debts.</li>
+                            <li class="lahiru_li">To enjoy your retirement without being a burden on anyone.</li>
                         </ul>
                     </div>
                 </div>
                 
                 <div class="column2">
                     <div class="left">
-                        <h3>LIFE INSURANCE</h3>
+                        <h3 class="lahiru_h3">LIFE INSURANCE</h3>
                         <p class="details">
                           A contract between an individual and an insurer where the insurer promises to pay 
                           the individual's listed beneficiaries a sum of money upon the death of the insured person.
                         </p>
                     </div>
                     <div class="left2">
-                        <h3>PREMIUM</h3>
+                        <h3 class="lahiru_h3">PREMIUM</h3>
                             <p class="details">
                                 The amount an insurance provider charges an individual to manage the risks related to the policy.
                             </p>
                     </div>
                     <div class="left3">
-                        <h3>DEATH BENEFIT</h3>
+                        <h3 class="lahiru_h3">DEATH BENEFIT</h3>
                             <p class="details">
                                 The amount a beneficiary receives when an insured individual dies.
                             </p>
                     </div>
 
                     <div class="left4">
-                        <h3>CLAIMS</h3>
+                        <h3 class="lahiru_h3">CLAIMS</h3>
                             <p class="details">
                                 The beneficiary (ies) files a death claim with the insurance company,&nbsp; submitting a certified copy 
                                 of the death certificate. Many companies allow insurers 30 days to review the claim. Then they can pay 
@@ -171,7 +205,7 @@
                     </div>
 
                     <div class="left5">
-                        <h3>MATURITY DATE</h3>
+                        <h3 class="lahiru_h3">MATURITY DATE</h3>
                             <p class="details">
                                 A life insurance policy reaches maturity upon the death of the policyholder or when the period stated 
                                 in the contract expires.
@@ -179,7 +213,7 @@
                     </div>
 
                     <div class="left6">
-                        <h3>BENEFICIARY</h3>
+                        <h3 class="lahiru_h3">BENEFICIARY</h3>
                             <p class="details">
                                 An individual who is entitled to receive the benefits of an insurance policy. The policyholder declares
                                 his or her beneficiaries upon signing the insurance contract.
@@ -187,7 +221,7 @@
                     </div>
 
                     <div class="left7">
-                        <h3>RIDER BENEFITS</h3>
+                        <h3 class="lahiru_h3">RIDER BENEFITS</h3>
                             <p class="details">
                                 A rider is an add-on provision to a basic insurance policy that provides additional benefits to the
                                 policyholder at an additional cost.
@@ -195,7 +229,7 @@
                     </div>
 
                     <div class="left8">
-                    <h3>RISKS</h3>
+                    <h3 class="lahiru_h3">RISKS</h3>
                         <p class="details">
                                 Insurance providers project the risks that may occur to a policyholder. The providers usually calculate 
                                 the risks based on factors such as age and medical history. Higher risk entails higher and more expensive coverage.
@@ -203,7 +237,7 @@
                     </div>
 
                     <div class="left9">
-                        <h3>ENDOWMENT</h3>
+                        <h3 class="lahiru_h3">ENDOWMENT</h3>
                             <p class="details">
                                 A type of insurance that provides benefits both when death occurs during the term of the policy and when the 
                                 insured individual is alive at the end of such a term 
@@ -212,6 +246,57 @@
 
                 </div>
             </div>
-            <script src="script\lifeScript.js"></script>
+
+            <div class="fot1">
+            <section data-bgimage="url(images/background/4.jpg) top" class="text-light">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 wow fadeInRight" data-wow-delay=".2s">
+                                <h2 class="lahiru_h2">Call us for further information. “Illusion  customer care is here to help you at anytime.</h2>
+                                <p class="lead id-color-secondary">We're available for 24 hours!</p>
+                            </div>
+
+                            <div class="col-md-6 text-lg-center text-sm-center wow fadeInRight" data-wow-delay=".4s">
+                                <div class="phone-num-big">
+                                    <i class="fa fa-phone"></i>
+                                    <span class="pnb-text id-color-secondary">
+                                        Call Us Now
+                                    </span>
+                                    <span class="pnb-num">
+                                        +94 200 333 800
+                                    </span>
+                                </div>
+                                <a href="#" class="btn-custom invert med">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+</div>
+
+                <a href="#" id="back-to-top"></a>
+
+            <div class="footer">
+            <?php include 'includes/footer.php' ?>
+            </div>
+            <script src="script/lifeScript.js"></script>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jpreLoader.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.isotope.min.js"></script>
+        <script src="js/easing.js"></script>
+        <script src="js/owl.carousel.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/enquire.min.js"></script>
+        <script src="js/jquery.stellar.min.js"></script>
+        <script src="js/jquery.plugin.js"></script>
+        <script src="js/typed.js"></script>
+        <script src="js/jquery.countTo.js"></script>
+        <script src="js/jquery.countdown.js"></script>
+        <script src="js/typed.js"></script>
+        <script src="js/designesia.js"></script>
+
     </body>
 </html>
