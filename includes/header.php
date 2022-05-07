2178ad38ddb9c0c406d001cddd1660a6f1e1f5f1
YehanIndrajith <101624557+YehanIndrajith@users.noreply.github.com>
2022-05-07 12:42:18 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title>Illuminating_Insurance</title>
+</head>
+<header class="transparent">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="de-flex sm-pt10">
+                                <div class="de-flex-col">
+                                    <!-- logo begin -->
+                                    <a href="index.php">
+                                    <div id="logo" class="header-logo">
+                                        
+                                    <img src="images/logo.png" alt="Description for image" width="150" height="50">                                       
+                                    </div>
+                                    </a>
+                                    <!-- logo close -->
+                                </div>
+                                <div class="de-flex-col header-col-mid">
+                                    <!-- mainmenu begin -->
+                                    <ul id="mainmenu">
+                                        <li>
+                                            <a href="index.php">Home<span></span></a>
+                                           
+                                        </li>
+                                        <li>
+                                            <a href="AboutUs.php" target="_blank">About Us<span></span></a>
+                                         
+                                        </li>
+                                        <li>
+                                            <a href="#">Insurance<span></span></a>
+                                            <ul>
+                                                <li><a href="life.php">Life Insurance</a></li>
+                                                <li><a href="index_motor.php" target="_blank">Motor Insurance</a></li>
+                                             
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">Login<span></span></a>
+                                            <ul>
+
+                                                <li><a href="register039.php">Sign Up</a></li>
+
+                                                <li><a href="login039.php">Sign in</a></li>
+
+                                                <li><a href="logout039.php">Sign out</a></li>
+                                               
+                                               <li><a href="account_profile039.php">My Plans</a></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="index.php#contact_us">Contact Us<span></span></a>
+                                           
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="de-flex-col">
+                                    <div class="h-phone"><span>Need&nbsp;Help?</span><i class="fa fa-phone id-color-secondary"></i> +94 200 300 900</div>
+                                    <span id="menu-btn"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+</html>
